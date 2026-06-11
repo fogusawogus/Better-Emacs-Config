6c863060de15be5f08e91567b950f3334afd3cf9
Jacob W <jacob.ka.wong@gmail.com>
2026-06-11 17:48:13 -0500

    Add stuff

--- post-init.el
+++ post-init.el
@@ -778,7 +778,10 @@
         "https://news.mit.edu/rss/research"
         "https://news.illinois.edu/feed/"
         "https://xkcd.com/rss.xml"
-        "https://www.pff.com/feed/teams/6"))
+        "https://www.pff.com/feed/teams/6"
+        "http://feeds.reuters.com/Reuters/PoliticsNews"
+        "http://feeds.reuters.com/reuters/entertainment"
+        "http://feeds.reuters.com/news/economy"))
 
 (use-package meson-mode
   :ensure t)
@@ -808,3 +811,37 @@
   :after corfu
   :config
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+(use-package auto-package-update
+  :custom
+  ;; Set the number of days between automatic updates.
+  ;; Here, packages will only be updated if at least 7 days have passed
+  ;; since the last successful update.
+  (auto-package-update-interval 7)
+
+  ;; Suppress display of the *auto-package-update results* buffer after updates.
+  ;; This keeps the user interface clean and avoids unnecessary interruptions.
+  (auto-package-update-hide-results t)
+
+  ;; Automatically delete old package versions after updates to reduce disk
+  ;; usage and keep the package directory clean. This prevents the accumulation
+  ;; of outdated files in Emacs's package directory, which consume
+  ;; unnecessary disk space over time.
+  (auto-package-update-delete-old-versions t)
+
+  ;; Uncomment the following line to enable a confirmation prompt
+  ;; before applying updates. This can be useful if you want manual control.
+  ;; (auto-package-update-prompt-before-update t)
+
+  :config
+  ;; Run package updates automatically at startup, but only if the configured
+  ;; interval has elapsed.
+  (auto-package-update-maybe)
+
+  ;; Schedule a background update attempt daily at 10:00 AM.
+  ;; This uses Emacs' internal timer system. If Emacs is running at that time,
+  ;; the update will be triggered. Otherwise, the update is skipped for that
+  ;; day. Note that this scheduled update is independent of
+  ;; `auto-package-update-maybe` and can be used as a complementary or
+  ;; alternative mechanism.
+  (auto-package-update-at-time "10:00"))
